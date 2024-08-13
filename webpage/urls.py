f7a84c2ee2b630749125d189faa1942fe6fade76
korawi66 <kirawojanjum@gmail.com>
2024-08-13 12:08:05 +0700

    setall

--- webpage/urls.py
+++ webpage/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.indexPage, name="index"),
     path("about/", views.aboutUs, name="about"),
-    path("contact/", views.contactUs),
-    path('for/',views.forPage, name="for-page")
-
+    path("contact/", views.contactUs, name="contact"),
+    path('for/', views.forPage, name="for-page"),
+    path("card/", views.cardPage, name='card-page'),
+    path("color/", views.CardcolorPage, name='color-page')
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -10,7 +10,25 @@ def aboutUs (request):
 def contactUs (request):
     return render(request, "contact.html")
 
-def forPage (request):
-    return render (request)
+def forPage(request):
+    context = {}
+    lt = list(range(0, 100))
+    context["list"] = lt
+    return render(request, 'for_test.html',context)
 
+def cardPage(request):
+    context = {}
+    lt = list(range(0, 100))
+    context["list"] = lt
+    return render(request, "card.html", context)
 
+def CardcolorPage(request):
+    context = {
+        'color': 'all',
+        
+    }
+    
+    if request.method == "GET" and request.GET.get('color') != None :
+        context['color'] = request.GET['color']
+        
+    return render(request, 'card_color.html', context)
