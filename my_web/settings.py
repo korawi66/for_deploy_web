692f1ceb0a2dc9a032ca346cb6249bb42bbe0445
korawi66 <kirawojanjum@gmail.com>
2024-08-13 12:00:56 +0700

    updatewsgi

--- my_web/settings.py
+++ my_web/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "my_web.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": [Path. joinpath (BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
